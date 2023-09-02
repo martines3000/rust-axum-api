1d682cb6203c7a2a2232afacec210f27c948d590
martines3000 <domajnko.martin@gmail.com>
2023-09-02 22:15:56 +0000

    feat: example monorepo with multiple apis

---
+++ bin/verifier/src/main.rs
@@ -0,0 +1,40 @@
+// Built-in Lints
+// Clippy lints
+#![allow(clippy::map_unwrap_or)]
+#![warn(
+    clippy::if_not_else,
+    clippy::items_after_statements,
+    clippy::mut_mut,
+    clippy::non_ascii_literal,
+    clippy::similar_names,
+    clippy::unicode_not_nfc,
+    clippy::used_underscore_binding,
+    missing_copy_implementations
+)]
+#![cfg_attr(test, allow(clippy::unwrap_used))]
+use std::net::SocketAddr;
+
+use axum::{routing::get, Json, Router};
+use serde_json::json;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new().route(
+        "/",
+        get(Json(json!({
+            "message": "Hello from verifier!",
+        }))),
+    );
+
+    // Run with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 5000));
+    tracing::debug!("Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
